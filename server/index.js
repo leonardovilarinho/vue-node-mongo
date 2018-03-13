0548e89569f10f7752b39a5b8a929bc4b7d3be4d
Leonardo Vilarinho <leonardo-i@outlook.com>
2018-03-13 13:38:01 -0300

    initial commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const app = require('./app')
+
+mongoose.connect(process.env.DB_PATH, (error) => {
+  if (error) throw error
+
+  app.listen(process.env.SERVER_PORT, () => console.log("Serving..."))
+})
