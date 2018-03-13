5eb310585ecfcab49f184b8075f23db690412f5a
Leonardo Vilarinho <leonardo-i@outlook.com>
2018-03-13 14:43:18 -0300

    create employee test

--- tests/server/employee.test.js
+++ tests/server/employee.test.js
@@ -9,9 +9,90 @@ afterAll(done => mongoose.connection.dropDatabase(done))
 it('Test mongoose connection', () => expect(mongoose.connection.readyState).toBe(1))
 
 describe('Test employees features', () => {
-  test('It should response the GET method', async () => {
-    const { text, statusCode} = await request(app).get('/')
+  test('Invalid register employee without name', async () => {
+    const employee = {lastname: 'Vilarinho', participation: 30}
+    const { text, statusCode} = await request(app).post('/employees').send(employee)
+
+    expect(statusCode).toBe(401)
+
+    const data = JSON.parse(text)
+    expect(data.error).toContain('nome')
+    expect(data.error).toContain('obrigatório')
+  })
+
+  test('Invalid register employee without lastname', async () => {
+    const employee = {name: 'Leonardo', participation: 30}
+    const { text, statusCode} = await request(app).post('/employees').send(employee)
+
+    expect(statusCode).toBe(401)
+
+    const data = JSON.parse(text)
+    expect(data.error).toContain('sobrenome')
+    expect(data.error).toContain('obrigatório')
+  })
+
+  test('Invalid register employee without pasticipation', async () => {
+    const employee = {name: 'Leonardo', lastname: 'Vilarinho'}
+    const { text, statusCode} = await request(app).post('/employees').send(employee)
+
+    expect(statusCode).toBe(401)
+
+    const data = JSON.parse(text)
+    expect(data.error).toContain('participação')
+    expect(data.error).toContain('obrigatória')
+  })
+
+  test('Invalid register employee with number in name', async () => {
+    const employee = {name: 'L3onardo', lastname: 'Vilarinho', participation: 30}
+    const { text, statusCode} = await request(app).post('/employees').send(employee)
+
+    expect(statusCode).toBe(401)
+
+    const data = JSON.parse(text)
+    expect(data.error).toContain('nome')
+    expect(data.error).toContain('número')
+  })
+
+  test('Invalid register employee with number in lastname', async () => {
+    const employee = { name: 'Leonardo', lastname: 'Vilar1nho', participation: 30 }
+    const { text, statusCode } = await request(app).post('/employees').send(employee)
+
+    expect(statusCode).toBe(401)
+
+    const data = JSON.parse(text)
+    expect(data.error).toContain('sobrenome')
+    expect(data.error).toContain('número')
+  })
+
+  test('Invalid register employee with participation < 0', async () => {
+    const employee = { name: 'Leonardo', lastname: 'Vilarinho', participation: -10 }
+    const { text, statusCode } = await request(app).post('/employees').send(employee)
+
+    expect(statusCode).toBe(401)
+
+    const data = JSON.parse(text)
+    expect(data.error).toContain('participação')
+    expect(data.error).toContain('0 e 100')
+  })
+
+  test('Invalid register employee with participation > 100', async () => {
+    const employee = { name: 'Leonardo', lastname: 'Vilarinho', participation: 101 }
+    const { text, statusCode } = await request(app).post('/employees').send(employee)
+
+    expect(statusCode).toBe(401)
+
+    const data = JSON.parse(text)
+    expect(data.error).toContain('participação')
+    expect(data.error).toContain('0 e 100')
+  })
+
+  test('Create an employee', async () => {
+    const employee = { name: 'Leonardo', lastname: 'Vilarinho', participation: 20 }
+    const { text, statusCode } = await request(app).post('/employees').send(employee)
 
     expect(statusCode).toBe(200)
+
+    const data = JSON.parse(text)
+    expect(data.error).toBeFalsy()
   })
 })
